3853f031e5058b89de606bf1004f8b0766585f81
Ra8t6y1 <Ramachandrarao_Tatavarty@gap.com>
2017-08-31 03:05:55 -0700

    [ramatatavarty] - adding reporting changes.

--- OmniAutomation/src/main/java/Scripts/LogintoGmail.java
+++ OmniAutomation/src/main/java/Scripts/LogintoGmail.java
@@ -1,7 +1,6 @@
 package Scripts;
 
 import org.junit.Test;
-
 import static ObjectRepository.LoginPage.email_text;
 import static ObjectRepository.LoginPage.logout_btn;
 import static ObjectRepository.LoginPage.next_btn;
@@ -13,6 +12,10 @@ import static com.omnitest.omni.Actions.openapplication;
 import static com.omnitest.omni.Actions.type;
 import static com.omnitest.omni.DriverFactory.getDriver;
 import static com.omnitest.omni.DriverFactory.initChromeDriver;
+import static com.omnitest.omni.Reporter.endReport;
+import static com.omnitest.omni.Reporter.logSuccess;
+import static com.omnitest.omni.Reporter.startReport;
+import static com.omnitest.omni.Reporter.takeScreenShot;
 
 /**
  * Created by vchilukuri on 8/28/17.
@@ -20,19 +23,29 @@ import static com.omnitest.omni.DriverFactory.initChromeDriver;
 public class LogintoGmail {
 
     @Test
-    public void logintest () throws InterruptedException {
-        initChromeDriver(System.getProperty("os.name").toLowerCase());
-        openapplication("https://www.gmail.com");
-        type(email_text, "omnitestautomation");
-        click(next_btn);
-        Thread.sleep(1000);
-        type(pass_text, "framework");
-        Thread.sleep(1000);
-        click(sign_btn);
-        Thread.sleep(1000);
-        click(sign_header_btn);
-        Thread.sleep(1000);
-        click(logout_btn);
-        getDriver().quit();
+    public void loginTest () throws Exception {
+        try {
+            startReport();
+            initChromeDriver(System.getProperty("os.name").toLowerCase());
+            openapplication("https://www.twitter.com");
+            logSuccess("gmail openend");
+            type(email_text, "omnitestautomation");
+            click(next_btn);
+            Thread.sleep(1000);
+            type(pass_text, "framework");
+            Thread.sleep(1000);
+            click(sign_btn);
+            Thread.sleep(1000);
+            logSuccess("login successful");
+            click(sign_header_btn);
+            Thread.sleep(1000);
+            click(logout_btn);
+            endReport();
+        }
+        catch (Exception e) {
+            takeScreenShot(getDriver(), "failed", e.toString());
+            endReport();
+        }
+
     }
 }
--- omniui-core/src/main/java/com/omnitest/omni/Reporter.java
+++ omniui-core/src/main/java/com/omnitest/omni/Reporter.java
@@ -1,33 +1,26 @@
 package com.omnitest.omni;
 
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import static com.omnitest.omni.DriverFactory.getDriver;
 
- 
 public class Reporter{
-	ExtentReports extent;
-	ExtentTest logger;
-	WebDriver driver;
-	
-	
-	@BeforeTest
-	public void startReport(){
-		extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/OMNIReport.html", true);
+	private static ExtentReports extent;
+	private static ExtentTest logger;
+
+	public static void startReport(){
+		extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/reports/"+"report"+new Date().toString()+".html");
+		logger = extent.startTest("Test Started");
 		extent
                 .addSystemInfo("Host Name", "OMNI REPORT")
                 .addSystemInfo("Environment", "Automation Testing")
@@ -35,34 +28,28 @@ public class Reporter{
                 extent.loadConfig(new File(System.getProperty("user.dir")+"Reporter-config.xml"));
 	}
 
-	public static String takeScreenShot(WebDriver driver, String screenshotName) throws Exception {
+	public static String takeScreenShot(WebDriver driver, String screenshotName, String error) throws Exception {
 		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File source = ts.getScreenshotAs(OutputType.FILE);
-		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/"+screenshotName+dateName+".png";
+		String destination = System.getProperty("user.dir") + "/test-output"+"/screenshots/"+screenshotName+dateName+".png";
 		File finalDestination = new File(destination);
 		FileUtils.copyFile(source, finalDestination);
+		logger.log(LogStatus.ERROR, error);
 		return destination;
 	}
-	
-	@AfterMethod
-	public void getResult(ITestResult result) throws Exception{
-		if(result.getStatus() == ITestResult.FAILURE){
-			logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
-			logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
-			String screenshotPath = Reporter.takeScreenShot(driver, result.getName());
-			logger.log(LogStatus.FAIL, logger.addScreenCapture(screenshotPath));
-		}else if(result.getStatus() == ITestResult.SKIP){
-			logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
-		}
 
+	public static void logSuccess(String stepName) {
+		logger.log(LogStatus.PASS, stepName);
+		MarkupHelper.createLabel(" Test Case PASSED", ExtentColor.GREEN);
+	}
+
+	public static void endReport() throws Exception{
+		getDriver().quit();
 		extent.endTest(logger);
+		extent.flush();
+		extent.close();
 	
 	}
-	
-	@AfterTest
-	public void endReport(){
-        extent.flush();
-         extent.close();
-    }
+
 }
