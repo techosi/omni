fd9287e887453893a7120458ea0dc456a570cae9
Ra8t6y1 <Ramachandrarao_Tatavarty@gap.com>
2017-09-03 23:21:05 -0700

    [ramatatavarty] - modifying to add testng capability.

--- OmniAutomation/src/main/java/Scripts/LogintoGmail.java
+++ OmniAutomation/src/main/java/Scripts/LogintoGmail.java
@@ -1,6 +1,8 @@
 package Scripts;
 
-import org.junit.Test;
+import com.omnitest.omni.ExtentReportsClass;
+import org.testng.annotations.Test;
+
 import static ObjectRepository.LoginPage.email_text;
 import static ObjectRepository.LoginPage.logout_btn;
 import static ObjectRepository.LoginPage.next_btn;
@@ -12,22 +14,19 @@ import static com.omnitest.omni.Actions.openapplication;
 import static com.omnitest.omni.Actions.type;
 import static com.omnitest.omni.DriverFactory.getDriver;
 import static com.omnitest.omni.DriverFactory.initChromeDriver;
-import static com.omnitest.omni.Reporter.endReport;
-import static com.omnitest.omni.Reporter.logSuccess;
-import static com.omnitest.omni.Reporter.startReport;
-import static com.omnitest.omni.Reporter.takeScreenShot;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by vchilukuri on 8/28/17.
  */
-public class LogintoGmail {
+public class LogintoGmail extends ExtentReportsClass {
 
     @Test
     public void loginTest () throws Exception {
-        try {
-            startReport();
+
             initChromeDriver(System.getProperty("os.name").toLowerCase());
             openapplication("https://www.gmail.com");
+            assertTrue(getDriver().getTitle().equals("Gmail"));
             logSuccess("gmail openend");
             type(email_text, "omnitestautomation");
             click(next_btn);
@@ -40,12 +39,12 @@ public class LogintoGmail {
             click(sign_header_btn);
             Thread.sleep(1000);
             click(logout_btn);
-            endReport();
-        }
-        catch (Exception e) {
-            takeScreenShot(getDriver(), "failed", e.toString());
-            endReport();
-        }
+    }
 
+    @Test
+    public void failTest () throws Exception {
+        initChromeDriver(System.getProperty("os.name").toLowerCase());
+        openapplication("https://www.twitter.com");
+        type(email_text, "omnitestautomation");
     }
 }
